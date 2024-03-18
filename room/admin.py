48b0d29db803f6c06afb43d4bdcd300da7aa5f88
Hojimurod3 <147169978+Hojimurod3@users.noreply.github.com>
2024-03-18 20:42:22 +0500

    feat: create model capacity

--- room/admin.py
+++ room/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Rooms_type, Rooms, Capacity
 
-# Register your models here.
+
+# Register your models here
+admin.site.register([Rooms_type, Rooms, Capacity])
--- room/models.py
+++ room/models.py
@@ -9,6 +9,9 @@ class Rooms_type(models.Model):
 
 class Rooms(models.Model):
     name = models.CharField(max_length=255)
-    type_id = models.ForeignKey(Rooms_type, on_delete=models.CASCADE, related_name='images')
-    capacity = models.IntegerField()
+    type_id = models.ForeignKey(Rooms_type, on_delete=models.CASCADE, related_name='rooms')
+    capacity_id = models.ForeignKey("Capacity", on_delete=models.SET_NULL, related_name='rooms', null=True)
 
+
+class Capacity(models.Model):
+    capacity = models.IntegerField()
